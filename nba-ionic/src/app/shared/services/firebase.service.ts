8e7aaaa82f3cb6ee1762434edf7b42a03021187f
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-02-29 20:14:00 +0100

    some feats in firebaseService

--- nba-ionic/src/app/shared/services/firebase.service.ts
+++ nba-ionic/src/app/shared/services/firebase.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { User } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  public isLogged: boolean = false;
+  public userLogged: User;
+  constructor(public fireAuth: AngularFireAuth) {
+    fireAuth.authState.subscribe(this.userIsLogued);
+  }
 
-  constructor() { }
+  userIsLogued(user: User) {
+    (user === null) ? this.isLogged = false : this.isLogged = true;
+  }
+
+  loginWithEmail({ email, password }: { email: string, password: string; }) {
+    this.fireAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(console.log)
+      .catch(console.error);
+  }
+
+  registerUser({ email, password }: { email: string, password: string; }) {
+    this.fireAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(console.log)
+      .catch(console.error);
+  }
 }
