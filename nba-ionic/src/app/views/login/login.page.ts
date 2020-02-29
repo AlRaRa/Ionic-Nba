14838577ee129755c09fcf02b0cb439f8287b7e9
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-02-29 21:51:32 +0100

    some refactor

--- nba-ionic/src/app/views/login/login.page.ts
+++ nba-ionic/src/app/views/login/login.page.ts
@@ -7,12 +7,12 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  private login: FormGroup;
+  protected login: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.login = this.formBuilder.group({
       email: ['', Validators.compose([Validators.maxLength(70), Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'), Validators.required])],
-      password: ['', Validators.required],
+      password: [''],
     });
   }
 
