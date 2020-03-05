96561de88f5dd5f68cb5810286cd7a38bdd43202
wirecobweb <wirecobweb@gmail.com>
2020-03-05 17:35:38 +0100

    generate component teams card

---
+++ nba-ionic/src/app/views/cards/card-team/card-team.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-team',
+  templateUrl: './card-team.component.html',
+  styleUrls: ['./card-team.component.scss'],
+})
+export class CardTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
