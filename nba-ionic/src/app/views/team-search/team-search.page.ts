c74dcd115816e0c099b11f36d09810c434abb1ed
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-03-07 15:31:50 +0100

    team modal created and working

--- nba-ionic/src/app/views/team-search/team-search.page.ts
+++ nba-ionic/src/app/views/team-search/team-search.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { TeamService } from 'src/app/shared/services/team/team.service';
+import { Team } from 'src/app/shared/models/team.model';
+import { ModalController } from '@ionic/angular';
+import { TeamModalPage } from '../team-modal/team-modal.page';
 
 @Component({
   selector: 'app-team-search',
@@ -11,10 +14,18 @@ export class TeamSearchPage implements OnInit {
 
   searchTerm$ = new Subject<string>();
 
-  constructor(private teamService: TeamService) { }
+  constructor(private teamService: TeamService, private modalController: ModalController) { }
 
   ngOnInit() {
     this.teamService.searchEntries(this.searchTerm$);
+  }
+
+  async onTeamSelected(team: Team) {
+    const modal = await this.modalController.create({
+      component: TeamModalPage,
+      componentProps: { team }
+    });
+    await modal.present();
   }
 
 
