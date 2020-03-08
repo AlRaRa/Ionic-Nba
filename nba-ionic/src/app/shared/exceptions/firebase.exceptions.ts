da474cf5fda3c7998042a2c758823a1549732b81
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-03-08 19:48:20 +0100

    add more exceptions

--- nba-ionic/src/app/shared/exceptions/firebase.exceptions.ts
+++ nba-ionic/src/app/shared/exceptions/firebase.exceptions.ts
@@ -2,5 +2,7 @@ export const EXCEPTIONS = {
     'auth/invalid-email': 'Email incorrecto',
     'auth/popup-closed-by-user': 'Ventana cerrada por usuario',
     'auth/user-not-found': 'Usuario no encontrado',
-    'auth/wrong-password': 'Contraseña incorrecta'
+    'auth/wrong-password': 'Contraseña incorrecta',
+    'auth/weak-password': 'La contraseña debe tener 6 o más caracteres',
+    'auth/email-already-in-use': 'Ese email ya está registrado'
 };
